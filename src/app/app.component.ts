d1f3c84c059fc0473029df6db7305c2f2a554c52
Giovanna Barros <giovanna.viecelli.2000@gmail.com>
2024-04-12 15:06:07 -0300

    create new site project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CanvasJSAngularChartsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'site';
+  chartOptions = {
+    title: {
+      text: "UMIDADE ATUAL"
+    },
+    data: [{
+      type: "column",
+      dataPoints: [
+        { label: "Apple", y: 10 },
+        { label: "Orange", y: 15 },
+        { label: "Banana", y: 25 },
+        { label: "Mango", y: 30 },
+        { label: "Grape", y: 28 }
+      ]
+    }]
+  };
 }
